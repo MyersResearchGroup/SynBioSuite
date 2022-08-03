afc4beed8fba54b40955a7977545b67b4bd52209
Zach Sents <zachsents@gmail.com>
2022-08-03 01:51:25 -0600

    Added badge to Results tab

--- src/components/panels/simulator/SimulatorPanel.jsx
+++ src/components/panels/simulator/SimulatorPanel.jsx
@@ -1,8 +1,11 @@
-import { Tabs } from '@mantine/core'
+import { Badge, Tabs } from '@mantine/core'
 import AnalysisWizard from './AnalysisWizard'
 import { createContext } from 'react'
 import AnalysisResults from './AnalysisResults'
 import PanelSaver from "../PanelSaver"
+import { useSelector } from 'react-redux'
+import { panelsSelectors } from '../../../redux/slices/panelsSlice'
+import { createSelector } from '@reduxjs/toolkit'
 
 
 export const PanelContext = createContext()
@@ -15,12 +18,22 @@ const TabValues = {
 
 export default function SimulatorPanel({ id }) {
 
+    const resultLength = useSelector(
+        createSelector(
+            state => panelsSelectors.selectById(state, id).results,
+            results => results && Object.keys(results).length
+        )
+    )
+
     return (
         <PanelContext.Provider value={id}>
             <Tabs defaultValue={TabValues.SETUP} styles={tabStyles}>
                 <Tabs.List>
                     <Tabs.Tab value={TabValues.SETUP}>Setup</Tabs.Tab>
-                    <Tabs.Tab value={TabValues.RESULTS}>Results</Tabs.Tab>
+                    <Tabs.Tab value={TabValues.RESULTS}>
+                        Results
+                        {resultLength && <Badge ml={10}>{resultLength}</Badge>}
+                    </Tabs.Tab>
                 </Tabs.List>
                 <Tabs.Panel value={TabValues.SETUP}>
                     <AnalysisWizard />
