bd66df577ef8c2f4b3d82bbcc696a9f74697c919
Zach Sents <zachsents@gmail.com>
2022-08-07 14:04:55 -0600

    Added browser compatibility catch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Activities from './components/activities/Activities'
 import Panels from './components/panels/Panels'
 import { NotificationsProvider } from "@mantine/notifications"
+import BrowserCompatiblityCatch from './components/BrowserCompatiblityCatch'
 
 export default function App() {
     return (
         <NotificationsProvider autoClose={5000} limit={8}>
             <Activities />
             <Panels />
+            <BrowserCompatiblityCatch />
         </NotificationsProvider>
     )
 }
