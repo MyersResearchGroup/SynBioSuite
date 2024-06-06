16bddf5b38c2333abcbc345e3b09ec08c50dfa60
Derick Sayavong <dsayavong54651@gmail.com>
2024-06-06 10:07:47 -0600

    Changed export name

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,15 +2,15 @@ import { configureStore } from '@reduxjs/toolkit'
 import panelsSlice from './slices/panelsSlice'
 import workingDirectorySlice from "./slices/workingDirectorySlice"
 import activitySlice from "./slices/activitySlice"
-import saveSlice from './slices/saveIndicatorSlice'
+import saveIndicatorReducer from './slices/saveIndicatorSlice'
 
 export default configureStore({
     reducer: {
         activities: activitySlice.reducer,
         workingDirectory: workingDirectorySlice.reducer,
         panels: panelsSlice.reducer,
-        saveIndicator: saveSlice
+        saveIndicator: saveIndicatorReducer
     }
 })
 
-export { panelsSlice, workingDirectorySlice, activitySlice, saveSlice }
+export { panelsSlice, workingDirectorySlice, activitySlice, saveIndicatorReducer }
