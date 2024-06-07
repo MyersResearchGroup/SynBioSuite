3dc5116c78f92e1dba755546b2f5cdec0d18096a
Derick Sayavong <dsayavong54651@gmail.com>
2024-06-07 14:20:02 -0600

    Moved the save indicator next to "Local Explorer" Label, also added in progress icon

--- src/components/saveIndicatorDisplay.jsx
+++ src/components/saveIndicatorDisplay.jsx
@@ -1,12 +1,17 @@
 import { CgCheckO } from "react-icons/cg";
 import Expire from "./Expire";
 import { useSelector } from "react-redux";
+import { MdDataSaverOff } from "react-icons/md";
+
 export default function SaveIndicatorDisplay() {
     const isSaving = useSelector(state => state.saveIndicator.isSaving)
     return (
         <>
             {isSaving ? 
-                "Saving..."
+                <span>
+                    Saving
+                    <MdDataSaverOff color="#FFFF00" style={{marginLeft: "5px"}}/>
+                </span>
                 : 
                 <Expire delay={3000}>
                     Saved
