1956269ba53fba181d977f13848c563dfd4a37dd
Derick Sayavong <dsayavong54651@gmail.com>
2024-06-08 13:12:15 -0600

    fixed content overflowing when too many tabs open

--- src/components/panels/Panels.jsx
+++ src/components/panels/Panels.jsx
@@ -4,7 +4,6 @@ import Panel from "./Panel"
 import CenteredTitle from "../CenteredTitle"
 import { useLocalStorage } from "@mantine/hooks"
 import WelcomeScreen from "../WelcomeScreen"
-import { useSelector } from "react-redux"
 
 export default function Panels() {
 
@@ -12,15 +11,12 @@ export default function Panels() {
     const panelIds = usePanelIds()
     const [activePanel, setActivePanel] = useActivePanel()
     const reorderPanels = useReorderPanels()
+    
     // first time visitor
     const [firstTime] = useLocalStorage({ key: 'first-time-visiting', defaultValue: true })
-
-    // see if activity panel is open, will be used to change max width of the div below
-    // doing so will prevent the content panels from being pushed around and stay centered even when the side panel is toggled off
-    const isActivityPanelOpen = useSelector(state => state.activities.active)
-
-    return (
-        <div style={{ flexGrow: 1, maxWidth: !!isActivityPanelOpen ?  "calc(100vw - 318px)" : "calc(100vw - 58px)" }}>
+   
+    return ( // width: 0 prevents content from overflowing when too many tabs are open
+        <div style={{flexGrow: 1, width: 0}}> 
             {panelIds.length ?
                 <DragTabs
                     tabComponent={Panel.Tab}
