ac92f93d518b8739283c13e8d79074f1aa70b248
KeremGurkan <103614702+KeremGurkan@users.noreply.github.com>
2024-11-21 14:29:25 -0700

    Dirty API testing XDC server

--- src/components/panels/xdc/SBHandFJLogIn.jsx
+++ src/components/panels/xdc/SBHandFJLogIn.jsx
@@ -16,6 +16,12 @@ export default function SBHandFJLogIn() {
 
     const [verifiedTokens, setVerifiedTokens] = usePanelProperty(panelId, 'verifiedToken', false);
 
+    const [experimentalId, setExperimentalId] = usePanelProperty(panelId, 'experimental', false)
+    const experimentalFile = useFile(experimentalId)
+
+    const [XDCdataID, setXDCDataID] = usePanelProperty(panelId, 'XDCdataID', false)
+    const xDCdataFile = useFile(XDCdataID)
+
     // modal handlers
     const [ addingSBHInstance, addingSBHInstanceHandler] = useDisclosure(false);
     const [ removingSBHInstance, removingSBHInstanceHandler] = useDisclosure(false);
@@ -68,6 +74,16 @@ export default function SBHandFJLogIn() {
                     title: 'Login Successful',
                     message: 'You have successfully logged in.',
                     color: 'green',
+                });
+                await axios.post('http://127.0.0.1:5000/upload_sbs', {
+                    Metadata: xDCdataFile,
+                    AuthToken: SBH_Token,
+                    Params: {"fj_url": "localhost:8000", "fj_user": "Gonza10V", "fj_pass": "010101", "sbh_url": "https://synbiohub.colorado.edu", "sbh_user": "Gonza10V", "sbh_pass": "010101", "sbh_collec": "xdc_sbs_test_uploader_file", "fj_overwrite": false, "sbh_overwrite": false}
+                }
+                , {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
                 });
             } else {
                 throw new Error('Invalid login response');
