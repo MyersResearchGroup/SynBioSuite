2501b68483a83ed30305bf94b7753440bf63b562
CoderLLamaPhone <kegu3575@colorado.edu>
2025-01-29 16:07:35 -0700

    Remvoed tempfile library

--- views.py
+++ views.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, jsonify
 
 import excel2sbol.converter as conv
 import sbol2
-#import tempfile
 import requests
 import os
 from excel2flapjack.main import X2F
