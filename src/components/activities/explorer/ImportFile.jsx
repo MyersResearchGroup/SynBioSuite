30600b6410f54cbdcd5ea1845fa5553628798ca6
Kenzo-Schwab <kenzoschwab@gmail.com>
2025-02-09 16:55:37 -0700

    Struggling to work on the dragging the file when its imported function

--- src/components/activities/explorer/ImportFile.jsx
+++ src/components/activities/explorer/ImportFile.jsx
@@ -2,32 +2,71 @@ import { Group } from "@mantine/core"
 import { AiOutlinePlus } from "react-icons/ai"
 import { getPrimaryColor } from "../../../modules/colorScheme"
 import { Text } from "@mantine/core"
+import { useState } from "react"
+import DragObject from "../../DragObject"
+import { titleFromFileName } from "../../../redux/hooks/workingDirectoryHooks"
+
+
 
 export default function ImportFile({onSelect, text}){
 
+    const [selectedFileName, setSelectedFileName] = useState("");
+   
+    const [file, setSelectedFile] = useState()
     
-
     const handleClick = async () => {
-        const directoryHandle = await window.showDirectoryPicker({
-            mode: 'readwrite',
-            startIn: 'desktop'
-        })
-        
-        onSelect?.(directoryHandle)
+        try{
+            const[fileHandle] = await window.showOpenFilePicker({
+                types: [
+                    {
+                        description: "OMEX or Edge HTML Files",
+                        accept: {
+                            "application/omex": [".omex"],
+                            "text/html": [".html", ".htm"],
+                            "application/xhtml+xml": [".xhtml"],
+                            "application/xml": [".xml"],
+                            "multipart/related": [".mhtml"]}
+                    }
+                ],
+                multiple: false,
+                startIn: 'desktop'
+            })
+
+            const fileName = fileHandle.name || (await fileHandle.getFile());
+            setSelectedFileName(fileName);
+            setSelectedFile(fileHandle)
+
+            onSelect?.(fileHandle);
+        } catch (err) {
+            console.error("File selection canceled or failed", err)
+        }
+
     }
 
+
 return (
+    <>
     <Group
         sx={groupStyle}
         onClick={() => handleClick()}
     >
         <AiOutlinePlus />
-        <Text size='sm' sx={textStyle}>{text}</Text>
+        <Text size='sm' sx={textStyle}>{selectedFileName || text}</Text>
     </Group>
+    <DragObject
+        title={titleFromFileName(file.name)}
+        fileId={fileId}
+        type={file.objectType}
+        icon={icon}
+        onDoubleClick={handleOpenFile}
+        onContextMenu={handleRightClick}
+     />
+    </>
 )
 
 
 }
+
 
 const groupStyle = theme => ({
     padding: '3px 0 3px 8px',
@@ -39,6 +78,7 @@ const groupStyle = theme => ({
     }
 })
 
+
 const textStyle = theme => ({
     flexGrow: 1,
     textOverflow: 'ellipsis',
@@ -47,5 +87,3 @@ const textStyle = theme => ({
     color: getPrimaryColor(theme, 5),
     fontWeight: 500,
 })
-
-    
