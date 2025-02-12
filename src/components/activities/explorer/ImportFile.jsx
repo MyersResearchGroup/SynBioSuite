7dc9fe9dab46a94d0b7d80e1ec5ab077fd220ddd
Kenzo-Schwab <kenzoschwab@gmail.com>
2025-02-11 20:06:56 -0700

    added the metadata function in attempts to be able to drag the file but now the file does not import

--- src/components/activities/explorer/ImportFile.jsx
+++ src/components/activities/explorer/ImportFile.jsx
@@ -1,89 +1,86 @@
-import { Group } from "@mantine/core"
-import { AiOutlinePlus } from "react-icons/ai"
-import { getPrimaryColor } from "../../../modules/colorScheme"
-import { Text } from "@mantine/core"
-import { useState } from "react"
-import DragObject from "../../DragObject"
-import { titleFromFileName } from "../../../redux/hooks/workingDirectoryHooks"
+import { Group } from "@mantine/core";
+import { AiOutlinePlus } from "react-icons/ai";
+import { getPrimaryColor } from "../../../modules/colorScheme";
+import { Text } from "@mantine/core";
+import { useState } from "react";
+import DragObject from "../../DragObject";
+import { titleFromFileName } from "../../../redux/hooks/workingDirectoryHooks";
 
+export default function ImportFile({ onSelect, text }) {
+        const [selectedFile, setSelectedFile] = useState(null);
 
-
-export default function ImportFile({onSelect, text}){
-
-    const [selectedFileName, setSelectedFileName] = useState("");
-   
-    const [file, setSelectedFile] = useState()
-    
-    const handleClick = async () => {
-        try{
-            const[fileHandle] = await window.showOpenFilePicker({
-                types: [
-                    {
-                        description: "OMEX or Edge HTML Files",
-                        accept: {
-                            "application/omex": [".omex"],
-                            "text/html": [".html", ".htm"],
-                            "application/xhtml+xml": [".xhtml"],
-                            "application/xml": [".xml"],
-                            "multipart/related": [".mhtml"]}
-                    }
-                ],
-                multiple: false,
-                startIn: 'desktop'
-            })
-
-            const fileName = fileHandle.name || (await fileHandle.getFile());
-            setSelectedFileName(fileName);
-            setSelectedFile(fileHandle)
-
-            onSelect?.(fileHandle);
-        } catch (err) {
-            console.error("File selection canceled or failed", err)
+        async function addFileMetadata(fileHandle) {
+            const file = await fileHandle.getFile(); 
+            return {
+                id: file.id, 
+                name: file.name,
+                objectType: await classifyFile(fileHandle) 
+            };
         }
 
+        const handleClick = async () => {
+            try {
+                const [fileHandle] = await window.showOpenFilePicker({
+                    types: [
+                        {
+                            description: "OMEX or Edge HTML Files",
+                            accept: {
+                                "application/omex": [".omex"],
+                                "text/html": [".html", ".htm"],
+                                "application/xhtml+xml": [".xhtml"],
+                                "application/xml": [".xml"],
+                                "multipart/related": [".mhtml"]
+                            }
+                        }
+                    ],
+                    multiple: false,
+                    startIn: 'desktop'
+                });
+
+                const fileMetadata = await addFileMetadata(fileHandle); 
+                setSelectedFile(fileMetadata);
+
+                onSelect?.(fileMetadata);
+            } catch (err) {
+                console.error("File selection canceled or failed", err);
+            }
+        };
+
+        return (
+            <Group sx={groupStyle} onClick={handleClick}>
+                {selectedFile && (
+                    <DragObject
+                        title={titleFromFileName(selectedFile.name)}
+                        fileId={selectedFile.id}
+                        type={selectedFile.objectType}
+                        icon={icon}
+                        onDoubleClick={handleOpenFile}
+                        onContextMenu={handleRightClick}
+                    />
+                )}
+                <AiOutlinePlus />
+                <Text size="sm" sx={textStyle}>
+                    {selectedFile?.name || text}
+                </Text>
+            </Group>
+        );
     }
 
+    const groupStyle = (theme) => ({
+        padding: "3px 0 3px 8px",
+        borderRadius: 3,
+        cursor: "pointer",
+        color: getPrimaryColor(theme, 5),
+        "&:hover": {
+            backgroundColor: theme.colors.dark[5]
+        }
+    });
 
-return (
-    <>
-    <Group
-        sx={groupStyle}
-        onClick={() => handleClick()}
-    >
-        <AiOutlinePlus />
-        <Text size='sm' sx={textStyle}>{selectedFileName || text}</Text>
-    </Group>
-    <DragObject
-        title={titleFromFileName(file.name)}
-        fileId={fileId}
-        type={file.objectType}
-        icon={icon}
-        onDoubleClick={handleOpenFile}
-        onContextMenu={handleRightClick}
-     />
-    </>
-)
-
-
-}
-
-
-const groupStyle = theme => ({
-    padding: '3px 0 3px 8px',
-    borderRadius: 3,
-    cursor: 'pointer',
-    color: getPrimaryColor(theme, 5),
-    '&:hover': {
-        backgroundColor: theme.colors.dark[5]
-    }
-})
-
-
-const textStyle = theme => ({
-    flexGrow: 1,
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
-    userSelect: 'none',
-    color: getPrimaryColor(theme, 5),
-    fontWeight: 500,
-})
+    const textStyle = (theme) => ({
+        flexGrow: 1,
+        textOverflow: "ellipsis",
+        whiteSpace: "nowrap",
+        userSelect: "none",
+        color: getPrimaryColor(theme, 5),
+        fontWeight: 500
+    });
