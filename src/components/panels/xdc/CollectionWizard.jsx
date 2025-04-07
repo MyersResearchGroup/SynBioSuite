c47c9641ac06fd6ee34014f1700ff46a4d8e02e1
CoderLLamaPhone <kegu3575@colorado.edu>
2025-04-06 21:33:50 -0600

    Updated XDC timeline

--- src/components/panels/xdc/CollectionWizard.jsx
+++ src/components/panels/xdc/CollectionWizard.jsx
@@ -1,31 +1,39 @@
-import { Container, Stepper, Group, Button, Space, Text, Title } from "@mantine/core"
+import { Container, Stepper, Group, Button, Space, Text, Avatar } from "@mantine/core"
 import Dropzone from '../../Dropzone'
-import CenteredTitle from '../../CenteredTitle'
 import { ObjectTypes } from '../../../objectTypes'
-import { titleFromFileName, useFile } from '../../../redux/hooks/workingDirectoryHooks'
+import { useFile } from '../../../redux/hooks/workingDirectoryHooks'
 import { useContext } from 'react'
 import { PanelContext } from './CollectionPanel'
 import { usePanelProperty } from '../../../redux/hooks/panelsHooks'
-import LoginForm from './SynBioHubLogIn'
-import ExperimentalTable from './ExperimentalTable'
 import XDCTimeline from './XDCTimeline'
 import { IoIosCloudUpload } from "react-icons/io";
-import { FaGear } from "react-icons/fa6";
 import { TbStatusChange } from "react-icons/tb";
-import Cookies from 'js-cookie';
-import SBHandFJLogIn from "./SBHandFJLogIn"
 import { RuntimeStatus } from "../../../runtimeStatus"
-
+import { useDispatch } from "react-redux"
+import { openSBH, openFJ } from "../../../redux/slices/loginModalSlice"
+import { useLocalStorage } from "@mantine/hooks"
 
 
 
 export default function CollectionWizard() {
     const panelId = useContext(PanelContext)
+    const dispatch = useDispatch()
+
+    const [dataSBH, setDataSBH] = useLocalStorage({ key: "SynbioHub", defaultValue: [] });
+    const [selectedSBH, setSelectedSBH] = useLocalStorage({ key: `SynbioHub-Primary`, defaultValue: "" });
+    const [dataFJ, setDataFJ] = useLocalStorage({ key: "Flapjack", defaultValue: [] });
+    const [selectedFJ, setSelectedFJ] = useLocalStorage({ key: `Flapjack-Primary`, defaultValue: "" });
+
+    const findInstance = (instance, repo) => {
+        if (repo == "SBH")
+            return dataSBH.find((element) => element.value === instance);
+        else if (repo == "FJ")
+            return dataFJ.find((element) => element.value === instance);
+    }
 
     // file info
     const fileHandle = usePanelProperty(panelId, "fileHandle")
-    const panelTitle = titleFromFileName(fileHandle.name)
-
+    
     // stepper states
     const numSteps = 4 
     const [activeStep, setActiveStep] = usePanelProperty(panelId, "activeStep", false, 0)
@@ -33,89 +41,107 @@ export default function CollectionWizard() {
     const prevStep = () => setActiveStep((current) => (current > 0 ? current - 1 : current))
     
     // Step 1: Experimental Metadata file
-    const [experimentalId, setExperimentalId] = usePanelProperty(panelId, 'experimental', false)
-    const experimentalFile = useFile(experimentalId)
+    const [metadataID, setMetadataID] = usePanelProperty(panelId, 'metadata', false)
+    const metadataFile = useFile(metadataID)
     
-    const handleExperimentalChange = name => {
-        setExperimentalId(name)
+    const handleMetadataChange = name => {
+        setMetadataID(name)
     }
 
-    const [XDCdataID, setXDCDataID] = usePanelProperty(panelId, 'XDCdataID', false)
-    const xDCdataFile = useFile(XDCdataID)
+    // Step 1: Experimental Results file
+    const [resultsID, setResultsID] = usePanelProperty(panelId, 'results', false)
+    const resultsFile = useFile(resultsID)
+    
     const handleExperimentalDataChange = name => {
-        setXDCDataID(name)
+        setResultsID(name)
     }
     
-    // Step 2: Checks to see if a verified token has been selected
-    const [verifiedToken, setVerifiedToken] = usePanelProperty(panelId, 'verifiedToken', false)
-
-    //Step 3: Timeline status--indicates XDC server's status
-    const [timelineStatus, setTimelineStatus] = usePanelProperty(panelId, "runtimeStatus", false, false, RuntimeStatus.COMPLETED);
-
-    //Step 4:
-    const [SBHUploaderInfo, setSBHUploadingInfo] = usePanelProperty(panelId, "SBHUploadingInfo", false);
-    const [FJUploaderInfo, setFJUploadingInfo] = usePanelProperty(panelId, "FJUploadingInfo", false);
-
-    //To be implemented
-    //Will be replaced with a switch case to handle proper movement between steps
-    let showNextButton = true
-    
-    //To hide items that are not yet implemented
-    let TBI = false
+    //Step 2: Timeline status--indicates XDC server's status
+    const [timelineStatus, setTimelineStatus] = usePanelProperty(panelId, "runtimeStatus", false, RuntimeStatus.WAITING);
 
     const getFileNameWithoutExtension = (fileName) => fileName.replace(/\.[^/.]+$/, "");
 
     return (
         <Container style={stepperContainerStyle}>
             <Stepper active={activeStep} onStepClick={setActiveStep} breakpoint="sm">
-                <Stepper.Step allowStepSelect={activeStep > 0 && activeStep != 3}
+                <Stepper.Step allowStepSelect={activeStep > 0 && activeStep != 2}
                     label="Upload Files"
                     description="Upload experimental data"
                     icon={<IoIosCloudUpload />}>
                     <Dropzone
                         allowedTypes={[ObjectTypes.Metadata.id]}
-                        item={experimentalFile?.name}
-                        onItemChange={handleExperimentalChange}>
+                        item={metadataFile?.name}
+                        onItemChange={handleMetadataChange}>
                         Drag & drop Experimental Metadata from the explorer
                     </Dropzone>
                     <Space h='lg' />
                     <Dropzone
-                        allowedTypes={[ObjectTypes.Output.id]}
-                        item={xDCdataFile?.name}
+                        allowedTypes={[ObjectTypes.Results.id]}
+                        item={resultsFile?.name}
                         onItemChange={handleExperimentalDataChange}>
-                        Drag & drop Plate Reader Output from the explorer
+                        Drag & drop Experimental Results from the explorer
                     </Dropzone>
                 </Stepper.Step>
 
-
                 <Stepper.Step
-                    allowStepSelect={activeStep > 1 && activeStep != 3}
-                    label="Log-In"
-                    description="For both SynBioHub and Flapjack"
-                    icon={<FaGear />}
-                >
-                    <Space h='lg' />
-                    <Group grow style={{ alignItems: 'flex-start' }}>
-                        <SBHandFJLogIn/>
-                    </Group>
-                </Stepper.Step>
-
-
-                <Stepper.Step
-                    allowStepSelect={activeStep > 2 && activeStep != 3}
-                    label="Upload Status"
-                    description="See your experiment uploaded"
+                    allowStepSelect={activeStep > 1 && activeStep != 2}
+                    label="Uploader Info"
+                    description="Upload your experiment here"
                     icon={<TbStatusChange />}
                 >
-                    <Space h='lg' />
-                    <Group grow style={{ alignItems: 'flex-start' }}>
-                        {TBI ? <ExperimentalTable /> : <></>}
-                        <XDCTimeline />
+                    <Group grow style={{ flexDirection: 'row', alignItems: 'flex-start' }}>
+                        <Group grow style={{ flexDirection: 'row', alignItems: 'flex-start' }} >
+                            <XDCTimeline />
+                            <Group grow style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
+                                <Group grow onClick={() => dispatch(openSBH())} style={{ alignItems: 'center', width: '100%' }}>
+                                    <Avatar
+                                        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-2.png"
+                                        size={60}
+                                        radius="md"
+                                        style={{
+                                            opacity: selectedSBH ? 1 : 0.5,
+                                            transition: 'opacity 0.1s',
+                                        }}
+                                        onMouseEnter={(e) => e.currentTarget.style.opacity = '0.7'}
+                                        onMouseLeave={(e) => e.currentTarget.style.opacity = selectedSBH ? '1' : '0.5'}
+                                    />
+                                    <div style={{ flex: 1, whiteSpace: 'nowrap', overflow: 'visible' }}>
+                                        <Text fz="md" fw={500}>
+                                            {selectedSBH ? findInstance(selectedSBH, "SBH").username : "Not Logged In"}
+                                        </Text>
+                                        <Text fz="xs" fw={700} c="dimmed">
+                                            {selectedSBH ? findInstance(selectedSBH, "SBH").instance : "Not Logged In"}
+                                        </Text>
+                                    </div>
+                                </Group>
+                                <Group grow onClick={() => dispatch(openFJ())} style={{ alignItems: 'center', width: '100%' }}>
+                                    <Avatar
+                                        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-2.png"
+                                        size={60}
+                                        radius="md"
+                                        style={{
+                                            opacity: selectedFJ ? 1 : 0.5,
+                                            transition: 'opacity 0.1s',
+                                        }}
+                                        onMouseEnter={(e) => e.currentTarget.style.opacity = '0.7'}
+                                        onMouseLeave={(e) => e.currentTarget.style.opacity = selectedFJ ? '1' : '0.5'}
+                                    />
+                                    <div style={{ flex: 1, whiteSpace: 'nowrap', overflow: 'visible' }}>
+                                        <Text fz="md" fw={500}>
+                                            {selectedFJ ? findInstance(selectedFJ, "FJ").username : "Not Logged In"}
+                                        </Text>
+                                        <Text fz="xs" fw={700} c="dimmed">
+                                            {selectedFJ ? findInstance(selectedFJ, "FJ").instance : "Not Logged In"}
+                                        </Text>
+                                    </div>
+                                </Group>
+                            </Group>
+                        </Group>
                     </Group>
                 </Stepper.Step>
 
                 <Stepper.Completed>
-                    <Text size="xl">
+                    {false ? <><Text size="xl">
                         The link to your SynBioHub collection can be found at:
                         <br />
                         <a href={`${SBHUploaderInfo?.instance}/user/${SBHUploaderInfo?.user}/${getFileNameWithoutExtension(fileHandle.name)}/${getFileNameWithoutExtension(fileHandle.name)}_collection/1`} style={{ textDecoration: 'none' }}>
@@ -137,14 +163,18 @@ export default function CollectionWizard() {
                     <Text size="md">
                         This experiment was uploaded to Flapjack by the user, {FJUploaderInfo?.name} (Also known by their username as {FJUploaderInfo?.user})
                         The known email is <a href={`mailto:${FJUploaderInfo?.email}`} style={{ textDecoration: 'none' }}>{FJUploaderInfo?.email}</a>
-                    </Text>
+                    </Text></> : <></>}
                     <hr />
                     <Text size="md">
                         Files uploaded:
                         <br />
-                        Metadata: {experimentalFile?.name}
+                        Metadata: {metadataFile?.name}
                         <br />
-                        Plate Reader Output: {xDCdataFile?.name}
+                        {resultsFile?.name ?
+                            <>Plate Reader Output: {resultsFile?.name}</>
+                        :
+                            <></>
+                        }
                     </Text>
                     <hr />
                     <Text size="xs" ta="center" fs="italic">
@@ -157,14 +187,68 @@ export default function CollectionWizard() {
                 <Button
                     variant="default"
                     onClick={prevStep}
-                    sx={{ display: activeStep === 0 || activeStep === 3 ? 'none' : 'block' }}
+                    sx={{ display: activeStep === 0 || activeStep === 2 ? 'none' : 'block' }}
                 >
                     Back
                 </Button>
-                {activeStep < 3 && experimentalId && XDCdataID && (activeStep !== 1 || verifiedToken) && (activeStep !== 2 || timelineStatus === RuntimeStatus.COMPLETED) ? (
+                {activeStep == 1 && timelineStatus == RuntimeStatus.WAITING ? (
+                    <>
+                        <Button
+                            onClick={() => {
+                                setTimelineStatus(RuntimeStatus.PROCESSING);
+                                setTimeout(() => {
+                                    setTimelineStatus(RuntimeStatus.UPLOADING);
+                                    setTimeout(() => {
+                                        setTimelineStatus(RuntimeStatus.COMPLETED);
+                                        setTimeout(() => nextStep(), 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }}
+                            disabled={!selectedSBH}
+                            sx={{ display: 'block', opacity: selectedSBH ? 1 : 0.5 }}
+                        >
+                            Upload to Synbiohub
+                        </Button>
+                        <Button
+                            onClick={() => {
+                                setTimelineStatus(RuntimeStatus.PROCESSING);
+                                setTimeout(() => {
+                                    setTimelineStatus(RuntimeStatus.UPLOADING);
+                                    setTimeout(() => {
+                                        setTimelineStatus(RuntimeStatus.COMPLETED);
+                                        setTimeout(() => nextStep(), 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }}
+                            disabled={!selectedFJ}
+                            sx={{ display: 'block', opacity: selectedFJ ? 1 : 0.5 }}
+                        >
+                            Upload to Flapjack
+                        </Button>
+                        <Button
+                            onClick={() => {
+                                setTimelineStatus(RuntimeStatus.PROCESSING);
+                                setTimeout(() => {
+                                    setTimelineStatus(RuntimeStatus.UPLOADING);
+                                    setTimeout(() => {
+                                        setTimelineStatus(RuntimeStatus.COMPLETED);
+                                        setTimeout(() => nextStep(), 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }}
+                            disabled={!selectedSBH || !selectedFJ}
+                            sx={{ display: 'block', opacity: selectedSBH && selectedFJ ? 1 : 0.5 }}
+                        >
+                            Upload to both Synbiohub and Flapjack
+                        </Button>
+                    </>
+                ) : (
+                    <></>
+                )}
+                {activeStep == 0 && metadataID ? (
                     <Button
                         onClick={nextStep}
-                        sx={{ display: showNextButton ? 'block' : 'none' }}
+                        sx={{ display: 'block' }}
                     >
                         Next step
                     </Button>
