3154193001bf1bab65b87133a430fa6d047e3607
Will Dravenstott <will.dravenstott@gmail.com>
2025-05-06 14:44:45 -0600

    Adding OpenAPI route (PATH/api/docs)

--- sbs_server/app/main.py
+++ sbs_server/app/main.py
@@ -1,11 +1,25 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Import views after creating app
 from . import views
+
+app = Flask(__name__)
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI
+API_URL = '/static/swagger.json'  # Where to find your OpenAPI spec
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={'app_name': "sbs_api"}
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 if __name__ == "__main__":
     # Only for debugging while developing
