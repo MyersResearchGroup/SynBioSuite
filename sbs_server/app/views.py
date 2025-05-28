964fe55fa649b892844693f49de59e3cfae6a91b
Gonzalo Vidal <35148159+Gonza10V@users.noreply.github.com>
2025-05-28 09:44:19 -0600

    Update views.py remove bad functions

--- sbs_server/app/views.py
+++ sbs_server/app/views.py
@@ -12,19 +12,13 @@ import sbol2
 
 #routes
 #check if the app is running
-@app.route('/api/pin')
+@app.route('/api/status')
 def pin():
     return jsonify({"status": "working"}), 200
 
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
-
-@app.route('/api/test_metadata_upload', methods = ['POST'])
-def test_metadata_upload():
-    if request.method == 'POST':
-        f = request.files['file']
-
 
 @app.route('/api/upload_sbs', methods = ['POST'])
 def upload_file_from_sbs_post():
@@ -69,48 +63,6 @@ def upload_file_from_sbs_post():
     }
     return jsonify(sbs_upload_response_dict)
     
-@app.route('/extract_data', methods=['POST'])
-def process_files():
-    if 'Metadata' not in request.files:
-        print(request)
-        return 'No file part', 400
-    metadata_file = request.files['Metadata']
-    if metadata_file.filename == '':
-        return 'No selected file', 400
-
-    if 'platereader_output' not in request.files:
-        print(request)
-        return 'No file part', 400
-    file = request.files['platereader_output']
-    if file.filename == '':
-        return 'No selected file', 400
-    # Get uploaded files from request
-    platereader_output_files = [file] #request.files.getlist('platereader_output') #[file] #TODO: see how to iterate through files 
-    # Get selected preset from request
-    preset = request.form.get('preset')
-
-    print(platereader_output_files)
-    print(preset)
-    # Call the process_files_with_script function
-    modified_file_path = process_files_with_script(platereader_output_files, metadata_file, preset)
-
-    # Return the modified file as a response
-    with open(modified_file_path, 'rb') as f:
-        modified_file_data = f.read()
-    #os.remove(modified_file_path)  # Optional: Remove the temporary file
-    print('finished')
-    # save modified file to disk
-
-
-
-    return modified_file_data
-
-@app.route('/return_info', methods=['POST'])
-def return_info():
-    # Return the profile information of the logged in user
-
-    
-    return
 
 
 @app.route('/sbol_2_build_golden_gate', methods=['POST'])
