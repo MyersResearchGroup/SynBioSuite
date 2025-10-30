f17de0ccc69070619b9a73be59718ecde4639bf4
Vincent Bowen <vibo3564@colorado.edu>
2025-10-30 16:30:52 -0600

    Refactor App and main to support multipage application

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,14 @@
-import Activities from './components/activities/Activities';
-import Panels from './components/panels/Panels';
-import { NotificationsProvider } from "@mantine/notifications";
-import BrowserCompatiblityCatch from './components/BrowserCompatiblityCatch';
-import LoginModal from './modules/modular_login/loginModal';
-import { useSelector, useDispatch } from 'react-redux';
-import { closeModal, closeSBH, closeFJ, closeAddSBHrepository, closeAddFJrepository, closeCreateCollection, closeSBHLogin } from './redux/slices/modalSlice';
-import FJModal from './modules/modular_login/FJModal';
-import SBHModal from './modules/modular_login/SBHModal';
-import AddSBHRepository from './modules/modular_login/addSBHRepository';
-import AddFJRepository from './modules/modular_login/addFJRepository';
-import CreateCollectionModal from './modules/CreateCollectionModal';
-import SBHOnly from './modules/modular_login/SBHOnly';
-import LandingPage from './components/LandingPage';
-
+import LandingPage from './pages/LandingPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CloudHome from './pages/CloudHome';
 
 export default function App() {
-    const loginModalOpened = useSelector((state) => state.modal.bothOpen);
-    const sbhModalOpened = useSelector((state) => state.modal.sbhOpen);
-    const fjModalOpened = useSelector((state) => state.modal.fjOpen);
-
-    const addSBHRepositoryOpened = useSelector((state) => state.modal.addSBHrepository);
-    const addFJRepositoryOpened = useSelector((state) => state.modal.addFJrepository)
-    
-    const collectionModalOpened = useSelector((state) => state.modal.addCollections)
-    
-    const SBHOnlyOpened = useSelector((state) => state.modal.sbhLoginOpen)
-
-    const libraryName = useSelector((state) => state.modal.libraryName)
-    const libraryDescription = useSelector((state) => state.modal.libraryDescription)
-
-    const dispatch = useDispatch();
-
     return (
-        <NotificationsProvider autoClose={5000} limit={8}>
-            <LandingPage />
-            {/* <Activities />
-            <Panels />
-            <BrowserCompatiblityCatch />
-            <LoginModal
-                opened={loginModalOpened}
-                onClose={() => dispatch(closeModal())}
-                repoName=""
-            />
-            <SBHModal
-                opened={sbhModalOpened}
-                onClose={() => dispatch(closeSBH())}
-                repoName=""
-            />
-            <FJModal
-                opened={fjModalOpened}
-                onClose={() => dispatch(closeFJ())}
-                repoName=""
-            />
-            <FJModal
-                opened={fjModalOpened}
-                onClose={() => dispatch(closeFJ())}
-                repoName=""
-            />
-            <AddSBHRepository 
-                opened={addSBHRepositoryOpened}
-                onClose={() => dispatch(closeAddSBHrepository())}
-            />
-            <AddFJRepository
-                opened={addFJRepositoryOpened}
-                onClose={() => dispatch(closeAddFJrepository())}
-            />
-            <CreateCollectionModal 
-                opened={collectionModalOpened}
-                libraryName={libraryName}
-                libraryDescription={libraryDescription}
-                onClose={() => dispatch(closeCreateCollection())}
-            />
-            <SBHOnly
-                opened={SBHOnlyOpened}
-                onClose={() => dispatch(closeSBHLogin())}
-            /> */}
-        </NotificationsProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/cloud-home" element={<CloudHome />} />
+                <Route path="/" element={<LandingPage />}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import { MantineProvider } from '@mantine/core'
 import TimeAgo from "javascript-time-ago"
 import en from "javascript-time-ago/locale/en.json"
 import { ModalsProvider } from '@mantine/modals'
+import { NotificationsProvider } from '@mantine/notifications'
 
 // const canvasBlue = ["#d6daee", "#c2c8e5", "#adb5dc", "#99a3d4", "#8490cb", "#707ec2", "#5b6bb9", "#4759b1", "#3246a8", "#2d3f97"]
 const theme = {
@@ -33,9 +34,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
             <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-                <ModalsProvider>
-                    <App />
-                </ModalsProvider>
+                <NotificationsProvider autoClose={5000} limit={8}>
+                    <ModalsProvider>
+                        <App />
+                    </ModalsProvider>
+                </NotificationsProvider>
             </MantineProvider>
         </Provider>
     </React.StrictMode>
