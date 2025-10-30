8ae2c745c9516a373082709a270728b63fdf1649
Vincent Bowen <vibo3564@colorado.edu>
2025-10-30 12:32:22 -0600

    Adds urls for Azure resource

--- iac/main.tf
+++ iac/main.tf
@@ -4,4 +4,9 @@ resource "azuread_application" "synbio-suite" {
   api {
     requested_access_token_version = 2
   }
+  web {
+    redirect_uris = [ "https://synbiosuite.org/cloud-home" ]
+    homepage_url = "https://synbiosuite.org/"
+    logout_url = "https://synbiosuite.org/cloud-logout"
+  }
 }
