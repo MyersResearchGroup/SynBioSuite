07aeb1c6125501efc3abdcef667ccd0ba67e7806
Vincent Bowen <vibo3564@colorado.edu>
2025-10-30 14:29:08 -0600

    Adds localhost as redirect uri for testing

--- iac/main.tf
+++ iac/main.tf
@@ -5,7 +5,7 @@ resource "azuread_application" "synbio-suite" {
     requested_access_token_version = 2
   }
   web {
-    redirect_uris = [ "https://synbiosuite.org/cloud-home" ]
+    redirect_uris = [ "https://synbiosuite.org/cloud-home", "http://localhost:3000/cloud-home" ]
     homepage_url = "https://synbiosuite.org/"
     logout_url = "https://synbiosuite.org/cloud-logout"
   }
