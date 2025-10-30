5d756daa1ce02a2ba1c1a5e098c225f9fe539115
Vincent Bowen <vibo3564@colorado.edu>
2025-10-30 16:51:51 -0600

    Adds local file system routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import LandingPage from './pages/LandingPage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CloudHome from './pages/CloudHome';
+import LocalHome from './pages/LocalHome';
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/cloud-home" element={<CloudHome />} />
-                <Route path="/" element={<LandingPage />}/>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/local-home" element={<LocalHome />} />
             </Routes>
         </BrowserRouter>
     );
