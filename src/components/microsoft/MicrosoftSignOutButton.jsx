2d680a11e262a67274a0498f42e29a6087ed9a35
Vincent Bowen <vibo3564@colorado.edu>
2025-10-30 22:15:02 -0600

    Adds sign out functionality

---
+++ src/components/microsoft/MicrosoftSignOutButton.jsx
@@ -0,0 +1,25 @@
+import { Text } from "@mantine/core";
+import { handleLogout } from "../../microsoft-utils/handleLogout";
+
+export default function MicrosoftSignOutButton() {
+    return (
+        <button
+            onClick={handleLogout}
+            style={{
+                display: "inline-flex",
+                alignItems: "center",
+                gap: "10px",
+                padding: "8px 16px",
+                borderRadius: "4px",
+                backgroundColor: "#d20c0cff",
+                cursor: "pointer",
+                color: "#fff",
+                marginTop: "10px"
+            }}
+        >
+            <Text>
+                Sign Out
+            </Text>
+        </button>
+    );
+}
