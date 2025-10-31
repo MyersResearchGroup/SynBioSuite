699fb5a1c86af9f126d97b5f956a3a6e72fced6d
Vincent Bowen <vibo3564@colorado.edu>
2025-10-30 20:20:57 -0600

    Handles Login and Logout better, and gets users name for display

---
+++ src/microsoft-utils/handleLogout.js
@@ -0,0 +1,4 @@
+import { msalInstance } from "./msal";
+
+export const handleLogout = async() =>
+  await msalInstance.logoutRedirect({});
--- src/pages/CloudHome.jsx
+++ src/pages/CloudHome.jsx
@@ -1,21 +1,75 @@
-import { Button, Center, Space, Stack, Text, Title, useMantineTheme } from '@mantine/core'
+import Activities from '../components/activities/Activities';
+import Panels from '../components/panels/Panels';
+import LoginModal from '../modules/modular_login/loginModal';
+import { useSelector, useDispatch } from 'react-redux';
+import { closeModal, closeSBH, closeFJ, closeAddSBHrepository, closeAddFJrepository, closeCreateCollection, closeSBHLogin } from '../redux/slices/modalSlice';
+import FJModal from '../modules/modular_login/FJModal';
+import SBHModal from '../modules/modular_login/SBHModal';
+import AddSBHRepository from '../modules/modular_login/addSBHRepository';
+import AddFJRepository from '../modules/modular_login/addFJRepository';
+import CreateCollectionModal from '../modules/CreateCollectionModal';
+import SBHOnly from '../modules/modular_login/SBHOnly';
+import MicrosoftPanels from '../components/microsoft/MicrosoftPanels';
 
+export default function LocalHome() {
+    // msalInstance.acquireTokenSilent(); 
+    const loginModalOpened = useSelector((state) => state.modal.bothOpen);
+    const sbhModalOpened = useSelector((state) => state.modal.sbhOpen);
+    const fjModalOpened = useSelector((state) => state.modal.fjOpen);
 
-export default function CloudHome() {
+    const addSBHRepositoryOpened = useSelector((state) => state.modal.addSBHrepository);
+    const addFJRepositoryOpened = useSelector((state) => state.modal.addFJrepository)
+    
+    const collectionModalOpened = useSelector((state) => state.modal.addCollections)
+    
+    const SBHOnlyOpened = useSelector((state) => state.modal.sbhLoginOpen)
 
-    const theme = useMantineTheme()
+    const libraryName = useSelector((state) => state.modal.libraryName)
+    const libraryDescription = useSelector((state) => state.modal.libraryDescription)
 
+    const dispatch = useDispatch();
     return (
-        <Center style={{
-            height: '100vh',
-            width: ' 100vw',
-            background: `radial-gradient(${theme.colors.dark[6]}, ${theme.colors.dark[7]})`
-        }}>
-            <Stack align='center'>
-                <Text>
-                    HELLO 
-                </Text>
-            </Stack>
-        </Center>
-    )
+        <>
+            <Activities />
+            <MicrosoftPanels />
+            <LoginModal
+                opened={loginModalOpened}
+                onClose={() => dispatch(closeModal())}
+                repoName=""
+            />
+            <SBHModal
+                opened={sbhModalOpened}
+                onClose={() => dispatch(closeSBH())}
+                repoName=""
+            />
+            <FJModal
+                opened={fjModalOpened}
+                onClose={() => dispatch(closeFJ())}
+                repoName=""
+            />
+            <FJModal
+                opened={fjModalOpened}
+                onClose={() => dispatch(closeFJ())}
+                repoName=""
+            />
+            <AddSBHRepository 
+                opened={addSBHRepositoryOpened}
+                onClose={() => dispatch(closeAddSBHrepository())}
+            />
+            <AddFJRepository
+                opened={addFJRepositoryOpened}
+                onClose={() => dispatch(closeAddFJrepository())}
+            />
+            <CreateCollectionModal 
+                opened={collectionModalOpened}
+                libraryName={libraryName}
+                libraryDescription={libraryDescription}
+                onClose={() => dispatch(closeCreateCollection())}
+            />
+            <SBHOnly
+                opened={SBHOnlyOpened}
+                onClose={() => dispatch(closeSBHLogin())}
+            />
+        </>
+    );
 }
