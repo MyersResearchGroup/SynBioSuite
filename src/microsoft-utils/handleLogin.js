a33d14f4fb975b7c3704530f799cdf0c1b9a4ef4
Vincent Bowen <vibo3564@colorado.edu>
2025-10-30 21:48:55 -0600

    Adds file scope in login call

--- src/microsoft-utils/handleLogin.js
+++ src/microsoft-utils/handleLogin.js
@@ -2,6 +2,6 @@ import { msalInstance } from "./msal";
 
 export const handleLogin = async () => {
   msalInstance.loginRedirect({
-    scopes: ["openid", "profile", "offline_access"],
+    scopes: ["openid", "profile", "Files.ReadWrite"],
   });
 };
