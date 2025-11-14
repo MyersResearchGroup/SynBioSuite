758c06372ba3d7a6e9a76242afc65074bd7d1fda
Ryan Greer <ryanrgreer@outlook.com>
2025-11-14 15:03:33 -0700

    displayId

--- sbs_server/app/utils.py
+++ sbs_server/app/utils.py
@@ -1,13 +1,15 @@
 import sbol2
 import sbol2build as s2b
-from typing import List
+from typing import List, Tuple
 
-def abstract_design_2_plasmids(abstract_design_uri: str, plasmid_collection_uri: str, plasmid_vector_uri: str, sbh: sbol2.PartShop) -> List[sbol2.Document]:
+def abstract_design_2_plasmids(abstract_design_uri: str, plasmid_collection_uri: str, plasmid_vector_uri: str, sbh: sbol2.PartShop) -> Tuple[List[sbol2.Document], sbol2.Document, str]:
     abstract_design_doc = sbol2.Document()
     sbh.pull(
         abstract_design_uri,
         abstract_design_doc
     )
+
+    abstract_design_id = s2b.abstract_translator.extract_toplevel_definition(abstract_design_doc).displayId
     
     plasmid_collection_doc = sbol2.Document()
     sbh.pull(
@@ -36,4 +38,18 @@ def abstract_design_2_plasmids(abstract_design_uri: str, plasmid_collection_uri:
         )
         part_documents.append(temp_doc)
 
-    return part_documents
+    return part_documents, backbone_doc, abstract_design_id
+
+def sbol2build_moclo(part_documents: List[sbol2.Document], backbone_doc: sbol2.document, abstract_design_id: str) -> sbol2.Document:
+    assembly_doc = sbol2.Document()
+    assembly_obj = s2b.golden_gate_assembly_plan(
+        f"{abstract_design_id}_assembly_plan",
+        part_documents,
+        backbone_doc,
+        "BsaI",
+        assembly_doc
+    )
+
+    composite_list = assembly_obj.run()
+
+    return assembly_doc
