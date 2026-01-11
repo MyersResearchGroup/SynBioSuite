8eaaa4419d5e71fcd2628e74a4ae9fbe7e070f67
Kerem Gurkan <kerem@Kerems-MacBook-Pro.local>
2026-01-11 15:19:49 -0700

    Updates import workflow

--- src/components/activities/explorer/ImportFile.jsx
+++ src/components/activities/explorer/ImportFile.jsx
@@ -2,30 +2,35 @@ import { Group } from "@mantine/core";
 import { AiOutlinePlus } from "react-icons/ai";
 import { getPrimaryColor } from "../../../modules/colorScheme";
 import { createContext, useState } from "react";
-import { classifyFile } from "../../../objectTypes";
+import { classifyFile, ObjectTypes } from "../../../objectTypes";
 import { Text } from "@mantine/core";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { writeToFileHandle } from "../../../redux/hooks/workingDirectoryHooks";
+import { useOpenPanel } from "../../../redux/hooks/panelsHooks";
+import { workingDirectorySlice } from "../../../redux/store";
 
 export const importedFile = createContext()
 
 export default function ImportFile({ onSelect, text, useSubdirectory = false }) {
         const [selectedFile, setSelectedFile] = useState(null)
         const dirName = useSelector(state => state.workingDirectory.directoryHandle)
+        const dispatch = useDispatch()
+        const openPanel = useOpenPanel()
+        const { actions } = workingDirectorySlice
 
 
 
         async function addFileMetadata(fileHandle) {
-            let directoryHandle = null
-            const file = await fileHandle.getFile()
-         
-            if( useSubdirectory ) {   
-                //const subdirectoryName = useSubdirectory 
-                try {
-                    //Checks to see if subdirectory exists already
-                    directoryHandle = await dirName.getDirectoryHandle(useSubdirectory, { create: false });        
-                } catch (e) {
-                    //If subdirectory does not exist, create it
-                    directoryHandle = await dirName.getDirectoryHandle(useSubdirectory, { create: true });
+            let directoryHandle = null;
+            const file = await fileHandle.getFile();
+
+            if (useSubdirectory) {
+                directoryHandle = await dirName.getDirectoryHandle(useSubdirectory, { create: false })
+                    .catch(() => dirName.getDirectoryHandle(useSubdirectory, { create: true }));
+
+                if (useSubdirectory === 'resources') {
+                    directoryHandle = await directoryHandle.getDirectoryHandle("uploads", { create: false })
+                        .catch(() => directoryHandle.getDirectoryHandle("uploads", { create: true }));
                 }
             }
 
@@ -34,10 +39,39 @@ export default function ImportFile({ onSelect, text, useSubdirectory = false }) 
                 name: file.name,
                 fileHandle: fileHandle,
                 directoryHandle: directoryHandle,
-                objectType: await classifyFile(fileHandle) 
-            }
+                objectType: await classifyFile(fileHandle)
+            };
         }
 
+        async function createResourceWorkflowJSON(fileName) {
+            try {
+                const resourcesDir = await dirName.getDirectoryHandle('resources', { create: true });
+                const baseFileName = fileName.replace(/\.[^/.]+$/, "");
+                const jsonFileName = `${baseFileName}.json`;
+                
+                const jsonFileHandle = await resourcesDir.getFileHandle(jsonFileName, { create: true });
+                
+                const defaultWorkflow = {
+                    activeStep: 0,
+                    files: [`resources/uploads/${fileName}`],
+                    collection: {},
+                    uploads: []
+                };
+                
+                await writeToFileHandle(jsonFileHandle, JSON.stringify(defaultWorkflow));
+                
+                jsonFileHandle.id = `resources/${jsonFileName}`;
+                jsonFileHandle.objectType = ObjectTypes.Resources.id;
+                
+                dispatch(actions.addFile(jsonFileHandle));
+                
+                openPanel(jsonFileHandle);
+                
+                console.log(`Created workflow JSON: ${jsonFileName}`);
+            } catch (err) {
+                console.error("Error creating resource workflow JSON:", err);
+            }
+        }
         
         const handleClick = async () => {
             try {
@@ -50,6 +84,9 @@ export default function ImportFile({ onSelect, text, useSubdirectory = false }) 
                 const fileMetadata = await addFileMetadata(fileHandle)
                 setSelectedFile(fileMetadata)
                 
+                if (useSubdirectory === 'resources') {
+                    await createResourceWorkflowJSON(fileMetadata.name);
+                }
 
                 onSelect?.(fileMetadata)
             } catch (err) {
