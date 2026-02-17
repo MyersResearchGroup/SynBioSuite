17bb910567d55008f50307e181e24c0c7f391c9c
Kerem Gurkan <kegu3575@colorado.edu>
2026-02-17 16:41:38 -0700

    Minor seqimprove implementation changes

--- frontend/src/components/panels/SeqImprovePanel.jsx
+++ frontend/src/components/panels/SeqImprovePanel.jsx
@@ -1,5 +1,4 @@
 import { createContext } from 'react'
-import PanelSaver from './PanelSaver'
 import { useSelector } from 'react-redux'
 import SeqImproveFrame from './SeqImproveFrame'
 
@@ -10,23 +9,6 @@ export default function SeqImprovePanel({fileObjectTypeId}) {
     return (
         <PanelContext.Provider value={activePanel}>
             <SeqImproveFrame/>
-            <PanelSaver id={activePanel} />
         </PanelContext.Provider>
     )
 }
-
-const centerStyle = {
-    minHeight: '50vh',
-    flexDirection: 'column'
-}
-
-const tabStyles = theme => ({
-    tabControl: {
-        width: 120,
-        textTransform: 'uppercase',
-        fontWeight: 600
-    },
-    tabsList: {
-        // backgroundColor: theme.colors.dark[6]
-    }
-})
