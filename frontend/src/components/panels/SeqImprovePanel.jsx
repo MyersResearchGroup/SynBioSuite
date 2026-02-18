ccd0924d2ba39827c1ff0ba4690ca50233c4944d
Kerem Gurkan <kegu3575@colorado.edu>
2026-02-17 17:00:46 -0700

    Adds multiple seqimprove panels

--- frontend/src/components/panels/SeqImprovePanel.jsx
+++ frontend/src/components/panels/SeqImprovePanel.jsx
@@ -1,13 +1,19 @@
 import { createContext } from 'react'
 import { useSelector } from 'react-redux'
 import SeqImproveFrame from './SeqImproveFrame'
+import { panelsSlice } from '../../redux/store'
+
+const { selectors } = panelsSlice
 
 export const PanelContext = createContext()
 
 export default function SeqImprovePanel({fileObjectTypeId}) {
-    const activePanel = useSelector(state => state.panels.active)
+    const activePanelId = useSelector(state => state.panels.active)
+    const panel = useSelector(state => selectors.selectById(state, activePanelId))
+    const url = panel?.url
+    
     return (
-        <PanelContext.Provider value={activePanel}>
+        <PanelContext.Provider value={url}>
             <SeqImproveFrame/>
         </PanelContext.Provider>
     )
