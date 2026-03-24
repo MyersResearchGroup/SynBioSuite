b71ffd609ae685285d9e3ef6e67502df95099cac
Travis Uhrig <travisformayor@users.noreply.github.com>
2026-03-24 09:38:30 -0600

    feat: detect Brave browser, fix compatibility component typo

--- frontend/src/pages/LocalHome.jsx
+++ frontend/src/pages/LocalHome.jsx
@@ -1,6 +1,6 @@
 import Activities from '../components/activities/Activities';
 import Panels from '../components/panels/Panels';
-import BrowserCompatiblityCatch from '../components/BrowserCompatiblityCatch';
+import BrowserCompatibilityCatch from '../components/BrowserCompatibilityCatch';
 import LoginModal from '../modules/modular_login/loginModal';
 import { useSelector, useDispatch } from 'react-redux';
 import { closeModal, closeSBH, closeFJ, closeAddSBHrepository, closeAddFJrepository, closeCreateCollection, closeSBHLogin } from '../redux/slices/modalSlice';
@@ -41,7 +41,7 @@ export default function LocalHome() {
         <>
             <Activities />
             <Panels />
-            <BrowserCompatiblityCatch />
+            <BrowserCompatibilityCatch />
             <LoginModal
                 opened={loginModalOpened}
                 onClose={() => dispatch(closeModal())}
