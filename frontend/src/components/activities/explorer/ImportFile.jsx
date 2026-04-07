f30e815b783c4bb9a0b6ec7a9d68cc6266c0d594
Kerem Gurkan <kegu3575@colorado.edu>
2026-04-07 01:05:47 -0600

    Adds modal to import workflow

--- frontend/src/components/activities/explorer/ImportFile.jsx
+++ frontend/src/components/activities/explorer/ImportFile.jsx
@@ -9,8 +9,9 @@ import { writeToFileHandle } from "../../../redux/hooks/workingDirectoryHooks";
 import { useOpenPanel } from "../../../redux/hooks/panelsHooks";
 import { workingDirectorySlice } from "../../../redux/store";
 import { showErrorNotification } from "../../../modules/util";
-import { useUnifiedModal } from "../../../redux/hooks/useUnifiedModal";
 import { upload_resource } from "../../../API";
+import { openUnifiedModal } from "../../../redux/slices/modalSlice";
+import { MODAL_TYPES } from "../../../modules/unified_modal/unifiedModal";
 
 export const importedFile = createContext()
 
@@ -32,131 +33,203 @@ async function getAvailableBaseName(objectTypeDir, uploadsDir, baseName, ext, ma
 }
 
 export default function ImportFile({ onSelect, text, useSubdirectory = false }) {
-        const [selectedFile, setSelectedFile] = useState(null)
-        const dirName = useSelector(state => state.workingDirectory.directoryHandle)
-        const dispatch = useDispatch()
-        const openPanel = useOpenPanel()
-        const { actions } = workingDirectorySlice
-        const { workflows } = useUnifiedModal()
+    const [selectedFile, setSelectedFile] = useState(null)
+    const dirName = useSelector(state => state.workingDirectory.directoryHandle)
+    const dispatch = useDispatch()
+    const openPanel = useOpenPanel()
+    const { actions } = workingDirectorySlice
 
-        async function addFileMetadata(fileHandle) {
-            const file = await fileHandle.getFile();
-            let directoryHandle = null;
+    const isWorkflowImport = WORKFLOW_SUBDIRS.includes(useSubdirectory)
 
-            if (useSubdirectory) {
-                directoryHandle = await dirName.getDirectoryHandle(useSubdirectory, { create: false })
-                    .catch(() => dirName.getDirectoryHandle(useSubdirectory, { create: true }));
+    async function addFileMetadata(fileHandle) {
+        const file = await fileHandle.getFile();
+        let directoryHandle = null;
 
-                // TODO: Automatically generate this
-                if (useSubdirectory === 'resources' || useSubdirectory === 'strains' || useSubdirectory === 'sampleDesigns') {
-                    directoryHandle = await directoryHandle.getDirectoryHandle("uploads", { create: false })
-                        .catch(() => directoryHandle.getDirectoryHandle("uploads", { create: true }));
-                }
-            }
+        if (useSubdirectory) {
+            directoryHandle = await dirName.getDirectoryHandle(useSubdirectory, { create: false })
+                .catch(() => dirName.getDirectoryHandle(useSubdirectory, { create: true }));
 
-            return {
-                fileobj: file,
-                name: file.name,
-                fileHandle: fileHandle,
-                directoryHandle,
-                objectType: await classifyFile(fileHandle)
-            };
-        }
-
-        async function saveFileToUploads(fileObj, objectType, actualFileName) {
-            const subDir = await dirName.getDirectoryHandle(objectType, { create: true });
-            const uploadsDir = await subDir.getDirectoryHandle('uploads', { create: true });
-            const fileHandle = await uploadsDir.getFileHandle(actualFileName, { create: true });
-            const writable = await fileHandle.createWritable();
-            const arrayBuffer = await fileObj.arrayBuffer();
-            await writable.write(arrayBuffer);
-            await writable.close();
-        }
-
-        async function createWorkflowJSON(availableBaseName, objectType, filePath, initialUpload) {
-            try {
-                const directory = await dirName.getDirectoryHandle(objectType, { create: true });
-                const jsonFileName = `${availableBaseName}.json`;
-                const jsonFileHandle = await directory.getFileHandle(jsonFileName, { create: true });
-
-                const defaultWorkflow = {
-                    activeStep: 0,
-                    file: filePath,
-                    collection: {},
-                    uploads: initialUpload ? [initialUpload] : []
-                };
-
-                await writeToFileHandle(jsonFileHandle, JSON.stringify(defaultWorkflow));
-
-                jsonFileHandle.id = `${objectType}/${jsonFileName}`;
-
-                if (objectType === 'resources') {
-                    jsonFileHandle.objectType = ObjectTypes.Resources.id;
-                } else if (objectType === 'strains') {
-                    jsonFileHandle.objectType = ObjectTypes.Strains.id;
-                } else if (objectType === 'sampleDesigns') {
-                    jsonFileHandle.objectType = ObjectTypes.SampleDesigns.id;
-                }
-
-                dispatch(actions.addFile(jsonFileHandle));
-                openPanel(jsonFileHandle);
-            } catch (err) {
-                console.error("Error creating resource workflow JSON:", err);
+            // TODO: Automatically generate this
+            if (useSubdirectory === 'resources' || useSubdirectory === 'strains' || useSubdirectory === 'sampleDesigns') {
+                directoryHandle = await directoryHandle.getDirectoryHandle("uploads", { create: false })
+                    .catch(() => directoryHandle.getDirectoryHandle("uploads", { create: true }));
             }
         }
 
-        const handleClick = async () => {
-            try {
-                const [fileHandle] = await window.showOpenFilePicker({
-                    types: [],
-                    multiple: false,
-                    startIn: 'desktop'
-                })
-
-                const fileMetadata = await addFileMetadata(fileHandle)
-                setSelectedFile(fileMetadata)
-                
-                // TODO: Automatically generate this list
-                if (useSubdirectory === 'resources' || useSubdirectory === 'strains' || useSubdirectory === 'sampleDesigns') {
-                    const uploadedFilePath = `${useSubdirectory}/uploads/${fileMetadata.name}`
-                    await createWorkflowJSON(fileMetadata.name, useSubdirectory, uploadedFilePath);
-                }
-
-                onSelect?.(fileMetadata)
-            } catch (err) {
-                console.warn("File selection canceled or failed", err)
-            }
-        }
-            
-
-        return (
-            <Group sx={groupStyle} onClick={handleClick}>
-                <importedFile.Provider value = {{selectedFile, setSelectedFile}}>
-                <AiOutlinePlus />
-                <Text size="sm" sx={textStyle} >
-                     {text}
-                 </Text> 
-                </importedFile.Provider>
-            </Group>
-        );
+        return {
+            fileobj: file,
+            name: file.name,
+            fileHandle: fileHandle,
+            directoryHandle,
+            objectType: await classifyFile(fileHandle)
+        };
     }
 
-    const groupStyle = (theme) => ({
-        padding: "3px 0 3px 8px",
-        borderRadius: 3,
-        cursor: "pointer",
-        color: getPrimaryColor(theme, 5),
-        "&:hover": {
-            backgroundColor: theme.colors.dark[5]
+    async function saveFileToUploads(fileObj, objectType, actualFileName) {
+        const subDir = await dirName.getDirectoryHandle(objectType, { create: true });
+        const uploadsDir = await subDir.getDirectoryHandle('uploads', { create: true });
+        const fileHandle = await uploadsDir.getFileHandle(actualFileName, { create: true });
+        const writable = await fileHandle.createWritable();
+        const arrayBuffer = await fileObj.arrayBuffer();
+        await writable.write(arrayBuffer);
+        await writable.close();
+    }
+
+    async function createWorkflowJSON(availableBaseName, objectType, filePath, initialUpload) {
+        try {
+            const directory = await dirName.getDirectoryHandle(objectType, { create: true });
+            const jsonFileName = `${availableBaseName}.json`;
+            const jsonFileHandle = await directory.getFileHandle(jsonFileName, { create: true });
+
+            const defaultWorkflow = {
+                activeStep: 0,
+                file: filePath,
+                collection: {},
+                uploads: initialUpload ? [initialUpload] : []
+            };
+
+            await writeToFileHandle(jsonFileHandle, JSON.stringify(defaultWorkflow));
+
+            jsonFileHandle.id = `${objectType}/${jsonFileName}`;
+
+            if (objectType === 'resources') {
+                jsonFileHandle.objectType = ObjectTypes.Resources.id;
+            } else if (objectType === 'strains') {
+                jsonFileHandle.objectType = ObjectTypes.Strains.id;
+            } else if (objectType === 'sampleDesigns') {
+                jsonFileHandle.objectType = ObjectTypes.SampleDesigns.id;
+            }
+
+            dispatch(actions.addFile(jsonFileHandle));
+            openPanel(jsonFileHandle);
+        } catch (err) {
+            console.error("Error creating resource workflow JSON:", err);
         }
-    });
+    }
 
-    const textStyle = (theme) => ({
-        flexGrow: 1,
-        textOverflow: "ellipsis",
-        whiteSpace: "nowrap",
-        userSelect: "none",
-        color: getPrimaryColor(theme, 5),
-        fontWeight: 500
-    });
+    async function runImportCollectionWorkflow() {
+        return new Promise((resolve) => {
+            dispatch(openUnifiedModal({
+                modalType: MODAL_TYPES.REPOSITORY_SELECTOR,
+                allowedModals: [
+                    MODAL_TYPES.REPOSITORY_SELECTOR,
+                    MODAL_TYPES.SBH_CREDENTIAL_CHECK,
+                    MODAL_TYPES.COLLECTION_BROWSER,
+                    MODAL_TYPES.ADD_SBH_REPO,
+                    MODAL_TYPES.SBH_LOGIN,
+                    MODAL_TYPES.CREATE_COLLECTION,
+                ],
+                props: {
+                    multiSelect: false,
+                    rootOnly: true,
+                },
+                callback: (result) => {
+                    resolve(result || null)
+                },
+            }))
+        })
+    }
 
+    const handleClick = async () => {
+        try {
+            const [fileHandle] = await window.showOpenFilePicker({
+                types: [],
+                multiple: false,
+                startIn: 'desktop'
+            })
+
+            const fileMetadata = await addFileMetadata(fileHandle)
+            setSelectedFile(fileMetadata)
+            
+            if (isWorkflowImport) {
+                const lastDot = fileMetadata.name.lastIndexOf('.')
+                const ext = lastDot >= 0 ? fileMetadata.name.slice(lastDot) : ''
+                const baseName = lastDot >= 0 ? fileMetadata.name.slice(0, lastDot) : fileMetadata.name
+
+                const objectTypeDir = await dirName.getDirectoryHandle(useSubdirectory, { create: true })
+                const uploadsDir = await objectTypeDir.getDirectoryHandle('uploads', { create: true })
+                const availableBaseName = await getAvailableBaseName(objectTypeDir, uploadsDir, baseName, ext)
+                const actualFileName = `${availableBaseName}${ext}`
+                const uploadedFilePath = `${useSubdirectory}/uploads/${actualFileName}`
+
+                const modalResult = await runImportCollectionWorkflow()
+                if (!modalResult?.completed) {
+                    return
+                }
+
+                const selectedCollection = modalResult.collections?.[0]
+                const selectedRepo = modalResult.selectedRepo
+                const authToken = modalResult.authToken
+
+                if (!selectedCollection?.uri || !selectedRepo || !authToken) {
+                    showErrorNotification("Import aborted", "Missing repository, credentials, or collection selection.")
+                    return
+                }
+
+                const collectionDisplayId = selectedCollection.uri.split('/').slice(-2, -1)[0]
+                    || selectedCollection.displayId
+                    || selectedCollection.name
+
+                await saveFileToUploads(fileMetadata.fileobj, useSubdirectory, actualFileName)
+
+                const uploadResponse = await upload_resource(
+                    uploadedFilePath,
+                    selectedRepo,
+                    authToken,
+                    collectionDisplayId,
+                    "",
+                    dirName,
+                    modalResult.sbh_overwrite ?? 0
+                )
+
+                const initialUpload = {
+                    collectionName: selectedCollection.name || selectedCollection.displayId || collectionDisplayId,
+                    uri: uploadResponse?.sbh_url || selectedCollection.uri,
+                    file: uploadedFilePath,
+                    date: new Date().toLocaleString(undefined, { timeZoneName: 'short' }),
+                    selectedRepo,
+                    userEmail: modalResult.userInfo?.email || null,
+                    type: 'initial',
+                }
+
+                await createWorkflowJSON(availableBaseName, useSubdirectory, uploadedFilePath, initialUpload)
+                return
+            }
+
+            onSelect?.(fileMetadata)
+        } catch (err) {
+            console.warn("File selection canceled or failed", err)
+        }
+    }
+        
+
+    return (
+        <Group sx={groupStyle} onClick={handleClick}>
+            <importedFile.Provider value = {{selectedFile, setSelectedFile}}>
+            <AiOutlinePlus />
+            <Text size="sm" sx={textStyle} >
+                    {text}
+                </Text> 
+            </importedFile.Provider>
+        </Group>
+    );
+}
+
+const groupStyle = (theme) => ({
+    padding: "3px 0 3px 8px",
+    borderRadius: 3,
+    cursor: "pointer",
+    color: getPrimaryColor(theme, 5),
+    "&:hover": {
+        backgroundColor: theme.colors.dark[5]
+    }
+});
+
+const textStyle = (theme) => ({
+    flexGrow: 1,
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+    userSelect: "none",
+    color: getPrimaryColor(theme, 5),
+    fontWeight: 500
+});
